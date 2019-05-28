3e61a79cb74ced5de1c6d708afc5ba0b16c7700d
fostralx <50956518+fostralx@users.noreply.github.com>
2019-05-28 15:39:51 +0300

    Update pl.ps1

--- pl.ps1
+++ pl.ps1
@@ -1,4 +1,4 @@
-$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+$PSDefaultParameterValues['*:Encoding'] = 'Ascii'
 $SMTPInfo = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587); 
 $SMTPInfo.EnableSsl = $true; 
 $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('digisparkbot', 'FB7knT5wBVdWNx9'); 
